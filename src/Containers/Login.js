7011de6b2288ca0f027a7342296f551dec2d3d13
Christopher <Christopher.fajardo73@gmail.com>
2020-01-30 11:31:05 -0500

    aws integration

--- src/Containers/Login.js
+++ src/Containers/Login.js
@@ -1,49 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { loginEmail, registerGoogle } from '../Redux/actions/actions';
 import { bindActionCreators } from 'redux';
-import { StyleSheet, ImageBackground, Text, View, Linking, TouchableOpacity } from 'react-native';
+import { StyleSheet, ImageBackground, Text, View, Linking, TouchableOpacity, Button } from 'react-native';
+import { Auth } from 'aws-amplify';
+
 // import config from '../../config';
 // console.log("Using API's URL => " + config.API_URL);
 // this.openURL('http://192.168.1.2:3001/api/nicapp/auth/google');
+// import statusCodes along with GoogleSignin
+import { GoogleSignin, GoogleSigninButton, statusCodes } from '@react-native-community/google-signin';
 
-class Login extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  loginWithGoogle = async () => {
-    let url = 'https://c-fajardo.com/api/nicapp/auth/google';
+// Somewhere in your code
 
-    Linking.canOpenURL(url)
-      .then(supported => {
-        if (!supported) {
-          console.log("Can't handle url: " + url);
-        } else {
-          return Linking.openURL(url).catch(err => console.error('An error occurred', err));
-        }
-      })
-      .catch(err => console.error('An error occurred', err));
-  };
-  render() {
-    const { user } = this.props.auth;
-    console.log('from LOGIN-PAGE ' + this.props.auth.isAuthenticated);
-    return (
-      <ImageBackground source={require('../assets/background.jpg')} style={styles.backgroundImage}>
-        <View style={styles.containerDiv}>
-          <View style={styles.titleDiv}>
-            <Text style={styles.titleText}>NORTH</Text>
-            <Text style={styles.titleText}>CENTRAL</Text>
-            <Text style={styles.titleText}>CONSULTING</Text>
-          </View>
-          <TouchableOpacity style={styles.btnGmail} onPress={this.loginWithGoogle}>
-            <Text style={styles.btnText}>Log in with Gmail</Text>
-          </TouchableOpacity>
+function Login() {
+  return (
+    <ImageBackground source={require('../assets/background.jpg')} style={styles.backgroundImage}>
+      <View style={styles.containerDiv}>
+        <View style={styles.titleDiv}>
+          <Text style={styles.titleText}>NORTH</Text>
+          <Text style={styles.titleText}>CENTRAL</Text>
+          <Text style={styles.titleText}>CONSULTING</Text>
         </View>
-      </ImageBackground>
-    );
-  }
+        <Button title="Sign in with Google" onPress={() => Auth.federatedSignIn({ provider: 'Google' })} />
+      </View>
+    </ImageBackground>
+  );
 }
-
+// <TouchableOpacity style={styles.btnGmail} onPress={login}>
+//         <Text style={styles.btnText}>Log in with Gmail</Text>
+//       </TouchableOpacity>
 const styles = StyleSheet.create({
   backgroundImage: {
     flex: 1,
